86f0b3abad1ef72d0e2e19c34ca4fd52ff417f77
Mikolaj Wichrowski <mikolajwichrowski@MacBook-Pro-van-Mikolaj.local>
2019-05-19 22:03:06 +0200

    main and lib setup

---
+++ src/deamon_lib.rs
@@ -0,0 +1,3 @@
+pub fn create() -> bool {
+    return true;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate clap;
+
+use clap::{Arg, App};
+// use std::path::Path;
+// use std::process;
+// use std::fs::File;
+// use std::io::{Read};
+
+mod deamon_lib;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("launchd-cli")
+      .version("0.1.0")
+      .author("MMW")
+      .about("A launchd cli tool written in Rust")
+      .arg(Arg::with_name("script")
+            .help("script to set.")
+            .empty_values(false)
+        )
+      .get_matches();
+    
+    if let Some(script_location) = matches.value_of("script") {
+        if deamon_lib::create() {
+            println!("{}", script_location);
+        }
+    }
 }
